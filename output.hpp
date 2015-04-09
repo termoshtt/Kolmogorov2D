2e15d0fc5de4a6f1ad894780f14e05d09daa621c
Toshiki Teramura <toshiki.teramura@gmail.com>
2015-04-09 18:42:27 +0900

    Implement pb output

--- output.hpp
+++ output.hpp
@@ -1,11 +1,54 @@
 #pragma once
 
 #include "Kolmogorov2D.hpp"
+#include "Kolmogorov2D.pb.h"
 #include <iostream>
 #include <iomanip>
 #include <fstream>
 
 namespace Kolmogorov2D {
+
+template <class Data> void serialize(const Data &data, std::string filename) {
+  std::ofstream ofs(filename,
+                    std::ios::out | std::ios::binary | std::ios::trunc);
+  if (!ofs)
+    throw std::runtime_error("Cannot open file: " + filename);
+  data.SerializeToOstream(&ofs);
+}
+
+template <typename T>
+void output(const Coefficient<T> &C, std::string filename) {
+  int Nx = C.size_x();
+  int Ny = C.size_y();
+  int stride = C.get_stride();
+  pb::Coefficient pbc;
+  pbc.set_nx(Nx);
+  pbc.set_ny(Ny);
+  for (int i = 0; i < Nx; i++) {
+    for (int j = 0; j < stride; j++) {
+      auto c = C.get(i, j);
+      pb::Complex *cc = pbc.add_value();
+      cc->set_real(c.real);
+      cc->set_imag(c.imag);
+    }
+  }
+  serialize(pbc, filename);
+}
+
+template <typename T> void output(const Field<T> &F, std::string filename) {
+  int Nx = F.size_x();
+  int Ny = F.size_y();
+  pb::Field pbf;
+  pbf.set_nx(Nx);
+  pbf.set_ny(Ny);
+  for (int i = 0; i < Nx; i++) {
+    for (int j = 0; j < Ny; j++) {
+      double val = F.get(i, j);
+      pbf.add_value(val);
+    }
+  }
+  serialize(pbf, filename);
+}
 
 template <class Data>
 void output_ascii(const Data &data, std::string filename) {
